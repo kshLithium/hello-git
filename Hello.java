0bf003e4f935e95799d1cb5d7b11a37ebc654d11
SungHun Kim <2071123@hansung.ac.kr>
2023-10-15 19:47:44 +0000

    c4:modify Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world hrom git!");
         System.out.println("modified");
+        System.out.println("changes after pushing to remote");
 	}
 }
